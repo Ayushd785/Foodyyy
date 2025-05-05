6bc279db358b869f396c7b35fa718d40acebb6b7
Ayushd785 <ayushd785@gmail.com>
2025-05-05 15:49:24 +0530

    Implement helper function for improved functionality - 2025-05-05

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -2,3 +2,10 @@
 function isEmptyObject(obj) {
     return Object.keys(obj).length === 0;
 }
+
+
+// Added 2025-05-05
+function isValidPhoneNumber(phone) {
+    const phoneRegex = /^\+?[\d\s\-\(\)]+$/;
+    return phoneRegex.test(phone) && phone.length >= 10;
+}
